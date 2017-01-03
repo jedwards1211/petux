f8f10c73846bab0d1d9e7707ad0f63d1ea962ff1
Vladimir Semyonov <tempname011@gmail.com>
2017-01-03 10:40:06 +0300

    Reworked implementation

--- src/index.js
+++ src/index.js
@@ -1,88 +1,62 @@
 import {
   createStore as reduxCreateStore,
-  combineReducers as reduxCombineReducers
+  combineReducers as reduxCombineReducers,
+  compose
 } from 'redux';
 
-function noOp() {}
+export function effectEnhancer(handler) {
+  return (createStore) => (reducer, preloadedState, enhancer) => {
+    let emittedEffects = [];
 
-function fastStrategy(performWith) {
-  return (dispatch) => ({
-    emit: performWith(dispatch),
-    dispatch,
-  });
-}
-
-function safeStrategy(performWith) {
-  return (innerDispatch) => {
-    const performForReal = performWith(dispatch);
-    let perform = noOp;
-
-    function dispatch(action) {
-      perform = performForReal;
-      
-      try {
-        innerDispatch(action);
-      } catch(e) {
-        perform = noOp;
-        throw e;
-      }
-
-      perform = noOp;
-    }
+    const lift = r => (state, action) => r(state, action, emit);
+    const replaceReducer = r => store.replaceReducer(lift(r));
+    const store = createStore(lift(reducer), preloadedState, enhancer);
+    const perform = handler(dispatch);
 
     function emit(effect) {
-      try {
-        perform(effect);
-      } catch(e) {
-        console.error(e);
+      emittedEffects.push(effect);
+    }
+
+    function dispatch(action) {
+      emittedEffects = [];
+      store.dispatch(action);
+      setTimeout(() => performAll(emittedEffects), 0);
+    }
+
+    function performAll(effects) {
+      for (let i = 0; i < effects.length; i++) {
+        try {
+          perform(effects[i]);
+        } catch(e) {
+          console.error(e);
+        }
       }
     }
 
-    return {
-      emit,
-      dispatch,
-    };
-  }
-}
-
-fastStrategy._isStrategy = true;
-safeStrategy._isStrategy = true;
-
-function effectEnhancer(strategy) {
-  return (createStore) => (reducer, preloadedState, enhancer) => {
-    let emit_ = noOp;
-    const lift = r => (state, action) => r(state, action, emit_);
-    const store = createStore(lift(reducer), preloadedState, enhancer);
-    const { emit, dispatch } = strategy(store.dispatch);
-    const replaceReducer = r => store.replaceReducer(lift(r));
-    emit_ = emit; // tie the knot.
     return {
       ...store,
       dispatch,
-      replaceReducer
+      replaceReducer,
     };
   }
 }
 
-function createStore(reducer, preloadedState, enhancer, strategy) {
-  if (!strategy._isStrategy) {
-    if (process.env.NODE_ENV === 'production') {
-      strategy = fastStrategy(strategy);
-    } else {
-      strategy = safeStrategy(strategy);
-    }
-  }
-  return effectEnhancer(strategy)(reduxCreateStore)(reducer, preloadedState, enhancer);
+export function createStore(reducer, preloadedState, enhancer, handler) {
+  enhancer = handler ? compose(effectEnhancer(handler), enhancer) : enhancer;
+  return (reduxCreateStore)(reducer, preloadedState, enhancer);
 }
 
-function combineReducers(reducers) {
-  let emit_ = noOp;
+export function combineReducers(reducers) {
+  let emit_;
+
   const lift = r => (state, action) => r(state, action, emit_);
   const liftedReducers = {};
   const keys = Object.keys(reducers);
+
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
     const value = reducers[key];
+
     if (typeof value === 'function') {
       liftedReducers[key] = lift(value);
     } else {
@@ -96,12 +70,4 @@ function combineReducers(reducers) {
     emit_ = emit;
     return combination(state, action);
   }
-}
-
-export {
-  createStore,
-  combineReducers,
-  fastStrategy,
-  safeStrategy,
-  effectEnhancer,
 }
