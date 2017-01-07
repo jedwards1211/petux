685dd205a75dd1ac6d0cee722f2bce6f14899f91
Vladimir Semyonov <tempname011@gmail.com>
2017-01-07 05:53:06 +0300

    Increase indentation in `examples/async`

--- examples/async/src/reducers/index.js
+++ examples/async/src/reducers/index.js
@@ -6,78 +6,76 @@ import {
 import { fetchPosts } from '../effects'
 
 export default function(emit) {
-// Note: the indentation is not increased here for better diff readability.
-const selectedReddit = (state = 'reactjs', action) => {
-  switch (action.type) {
-    case SELECT_REDDIT:
-      return action.reddit
-    default:
-      return state
+  const selectedReddit = (state = 'reactjs', action) => {
+    switch (action.type) {
+      case SELECT_REDDIT:
+        return action.reddit
+      default:
+        return state
+    }
   }
-}
 
-const shouldFetchPosts = (state) => {
-  if (!state) {
-    return true
+  const shouldFetchPosts = (state) => {
+    if (!state) {
+      return true
+    }
+    if (state.isFetching) {
+      return false
+    }
+    return state.didInvalidate
   }
-  if (state.isFetching) {
-    return false
-  }
-  return state.didInvalidate
-}
 
-const posts = (state = {
-  isFetching: false,
-  didInvalidate: true,
-  items: []
-}, action) => {
-  switch (action.type) {
-    case INVALIDATE_REDDIT:
-      return {
-        ...state,
-        didInvalidate: true
-      }
-    case REQUEST_POSTS:
-      if (shouldFetchPosts(state)) {
-        emit(fetchPosts(action.reddit))
+  const posts = (state = {
+    isFetching: false,
+    didInvalidate: true,
+    items: []
+  }, action) => {
+    switch (action.type) {
+      case INVALIDATE_REDDIT:
         return {
           ...state,
-          isFetching: true,
-          didInvalidate: false
+          didInvalidate: true
         }
-      } else {
+      case REQUEST_POSTS:
+        if (shouldFetchPosts(state)) {
+          emit(fetchPosts(action.reddit))
+          return {
+            ...state,
+            isFetching: true,
+            didInvalidate: false
+          }
+        } else {
+          return state
+        }
+      case RECEIVE_POSTS:
+        return {
+          ...state,
+          isFetching: false,
+          didInvalidate: false,
+          items: action.posts,
+          lastUpdated: action.receivedAt
+        }
+      default:
         return state
-      }
-    case RECEIVE_POSTS:
-      return {
-        ...state,
-        isFetching: false,
-        didInvalidate: false,
-        items: action.posts,
-        lastUpdated: action.receivedAt
-      }
-    default:
-      return state
+    }
   }
-}
 
-const postsByReddit = (state = { }, action) => {
-  switch (action.type) {
-    case INVALIDATE_REDDIT:
-    case RECEIVE_POSTS:
-    case REQUEST_POSTS:
-      return {
-        ...state,
-        [action.reddit]: posts(state[action.reddit], action)
-      }
-    default:
-      return state
+  const postsByReddit = (state = { }, action) => {
+    switch (action.type) {
+      case INVALIDATE_REDDIT:
+      case RECEIVE_POSTS:
+      case REQUEST_POSTS:
+        return {
+          ...state,
+          [action.reddit]: posts(state[action.reddit], action)
+        }
+      default:
+        return state
+    }
   }
-}
 
-return combineReducers({
-  postsByReddit,
-  selectedReddit
-})
-// See note about indentation above.
+  return combineReducers({
+    postsByReddit,
+    selectedReddit
+  })
 }
